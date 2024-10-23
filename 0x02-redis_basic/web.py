b44c021a43ebdcf977400c9d84373f21efe15e4f
Est-1y <terryiana22@gmail.com>
2024-10-23 10:19:36 +0000

    web cache

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Module
+module
 """
 import requests
 import redis
@@ -8,28 +8,34 @@ from functools import wraps
 
 store = redis.Redis()
 
-
 def count_url_access(method):
-    """ Url access count"""
+    """url access count"""
     @wraps(method)
     def wrapper(url):
         cached_key = "cached:" + url
         cached_data = store.get(cached_key)
+        
         if cached_data:
             return cached_data.decode("utf-8")
-
+        
         count_key = "count:" + url
-        html = method(url)
-
-        store.incr(count_key)
-        store.set(cached_key, html)
-        store.expire(cached_key, 10)
-        return html
+        try:
+            html = method(url)
+            store.incr(count_key)
+            store.set(cached_key, html, ex=10)
+            return html
+        except requests.RequestException as e:
+            return f"Error fetching {url}: {e}"
+    
     return wrapper
-
 
 @count_url_access
 def get_page(url: str) -> str:
-    """ url """
+    """url content"""
     res = requests.get(url)
+    res.raise_for_status()
     return res.text
+
+if __name__ == "__main__":
+    url = "http://slowwly.robertomurray.co.uk"
+    print(get_page(url))
