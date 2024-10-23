825c7cc39f12a4d7280d4b3d601acf8e93c23004
Est-1y <terryiana22@gmail.com>
2024-10-23 12:51:37 +0000

    web cache

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,41 +1,35 @@
 #!/usr/bin/env python3
-'''module
-'''
-import redis
+"""
+module
+"""
 import requests
+import redis
 from functools import wraps
-from typing import Callable
+
+store = redis.Redis()
 
 
-redis_store = redis.Redis()
+def count_url_access(method):
+    """url access count"""
+    @wraps(method)
+    def wrapper(url):
+        cached_key = "cached:" + url
+        cached_data = store.get(cached_key)
+        if cached_data:
+            return cached_data.decode("utf-8")
 
+        count_key = "count:" + url
+        html = method(url)
 
-def wrap_requests(fn: Callable) -> Callable:
-    """ wrapper """
-
-    @wraps(fn)
-    def wrapper(url: str) -> str:
-        """ Wrapper"""
-        redis_store.incr(f"count:{url}")
-        cached_response = redis_store.get(f"cached:{url}")
-        if cached_response:
-            return cached_response.decode('utf-8')
-
-        result = fn(url)
-        redis_store.setex(f"cached:{url}", 10, result)
-        return result
-
+        store.incr(count_key)
+        store.set(cached_key, html)
+        store.expire(cached_key, 10)
+        return html
     return wrapper
 
 
-@wrap_requests
+@count_url_access
 def get_page(url: str) -> str:
-    """url content
-    """
-    response = requests.get(url)
-    return response.text
-
-
-if __name__ == "__main__":
-    url = "http://slowwly.robertomurray.co.uk"
-    print(get_page(url))
+    """contents of a url """
+    res = requests.get(url)
+    return res.text
